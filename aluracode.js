0bf895d6f5763343bcb9f8d072ab201b9317a104
Biell <91361036+Biell666@users.noreply.github.com>
2024-09-30 07:56:50 -0300

    atualização de código geral
    melhorias e otimização

--- aluracode.js
+++ aluracode.js
@@ -1,17 +1,20 @@
 javascript:(function() {
     'use strict';
 
-    console.log("--- ALURA DESTROYER BY Biell ---");
+    console.log("--- ALURA DESTROYER V2 BY Biell ---");
 
+    // Mantém a modificação da marca d'água
     const waterMark = document.querySelector('.formattedText');
     if (waterMark) {
-        waterMark.innerHTML = 'Alura destroyer by Biell';
+        waterMark.innerHTML = 'Alura destroyer V2 by Biell';
     }
 
+    // Obtém cookies e URL atual
     let cookies = document.cookie;
     let actualUrl = window.location.href;
     let nextLessonButton = document.getElementsByClassName("bootcamp-next-button")[0];
 
+    // Verifica se o botão da próxima lição existe
     if (nextLessonButton) {
         let nextLessonLink = nextLessonButton.getAttribute('href');
         let parts = actualUrl.split('/');
@@ -19,6 +22,7 @@ javascript:(function() {
         let lessonId = parts[6];
         console.log(`[DEBUG] Lesson_Name: ${lessonName} Lesson_Id: ${lessonId}`);
 
+        // Requisição para marcar a lição como concluída
         fetch(`https://cursos.alura.com.br/course/${lessonName}/task/${lessonId}/mark-video`, {
             method: 'POST',
             credentials: 'include',
@@ -28,10 +32,14 @@ javascript:(function() {
             }
         }).then(data => {
             console.log("[DEBUG] Lesson Done!");
+            
+            // Clica imediatamente no botão da próxima lição
             nextLessonButton.click();
+        }).catch(error => {
+            console.error("[DEBUG] Falha ao marcar a lição:", error);
         });
 
     } else {
-        alert("Next Lesson Button not found 😦 Are you sure you are on the correct page?");
+        alert("Botão da próxima lição não encontrado 😦 Verifique se você está na página correta.");
     }
 })();
